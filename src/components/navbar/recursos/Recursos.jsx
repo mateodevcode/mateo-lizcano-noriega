53ad3adccbbf947b42f767eafbc66620e5512eca
Mateo Lizcano <149150878+mateodevcode@users.noreply.github.com>
2024-08-13 23:11:56 +0200

    Se agrega un nuevo recurso a la barra de navegación

--- src/components/navbar/recursos/Recursos.jsx
+++ src/components/navbar/recursos/Recursos.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { recursos } from "@/data/recursos.navbar";
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
@@ -14,55 +15,15 @@ const Recursos = ({ onClose }) => {
       >
         Recursos
       </MenuButton>
-      <MenuList className="bg-blue-800 dark:bg-green-900 text-white rounded-md mt-2 w-60">
-        <MenuItem className="dark:hover:bg-green-600/20 hover:bg-blue-600/20 rounded-md">
-          <Link
-            href={"/aprende-ingles"}
-            className="mx-2 flex flex-row justify-between items-center w-full"
-            onClick={onClose}
-          >
-            <span className="mx-2 my-1">Aprende Ingles</span>
-            <Image
-              alt="Tetris"
-              src={"/logo/logo.png"}
-              width={50}
-              height={50}
-              className="w-5"
-            />
-          </Link>
-        </MenuItem>
-        <MenuItem className="dark:hover:bg-green-600/20 hover:bg-blue-600/20 rounded-md">
-          <Link
-            href={"/aprende-programacion"}
-            className="mx-2 flex flex-row justify-between items-center w-full"
-            onClick={onClose}
-          >
-            <span className="mx-2 my-1">Aprende Programación</span>
-            <Image
-              alt="Tetris"
-              src={"/logo/logo.png"}
-              width={50}
-              height={50}
-              className="w-5"
-            />
-          </Link>
-        </MenuItem>
-        <MenuItem className="dark:hover:bg-green-600/20 hover:bg-blue-600/20 rounded-md">
-          <Link
-            href={"/aprende-ciberseguridad"}
-            className="mx-2 flex flex-row justify-between items-center w-full"
-            onClick={onClose}
-          >
-            <span className="mx-2 my-1">Aprende Ciberseguridad</span>
-            <Image
-              alt="Tetris"
-              src={"/logo/logo.png"}
-              width={50}
-              height={50}
-              className="w-5"
-            />
-          </Link>
-        </MenuItem>
+      <MenuList className="bg-blue-800 dark:bg-green-900 text-white rounded-md mt-2 w-66">
+        {recursos.map((recurso, index) => (
+          <MenuItem key={index} className="dark:hover:bg-green-600/20 hover:bg-blue-600/20 rounded-md">
+            <Link href={recurso.LinkTo} className="mx-2 flex flex-row justify-between items-center w-full" onClick={onClose}>
+              <span className="mx-2 my-1">{recurso.nombre}</span>
+              <Image alt={recurso.nombre} src={recurso.logo} width={50} height={50} className="w-5" />
+            </Link>
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
---
+++ src/data/recursos.navbar.js
@@ -0,0 +1,22 @@
+export const recursos = [
+  {
+    nombre: "One Second CyberSecurity",
+    LinkTo: "https://one-second-cybersecurity.vercel.app/",
+    logo: "/logo/one-second.png",
+  },
+  {
+    nombre: "Aprende Inglés",
+    LinkTo: "/aprende-ingles",
+    logo: "/logo/logo.png",
+  },
+  {
+    nombre: "Aprende Programación",
+    LinkTo: "/aprende-programacion",
+    logo: "/logo/logo.png",
+  },
+  {
+    nombre: "Aprende Ciberseguridad",
+    LinkTo: "/aprende-ciberseguridad",
+    logo: "/logo/logo.png",
+  },
+];
